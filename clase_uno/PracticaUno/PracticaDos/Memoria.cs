1e293816991250176087f6d3f7b87d056ea088f5
Jesus Roberto Saucedo <jr@devsigners.mx>
2020-02-21 12:58:03 -0700

    Memoria

---
+++ clase_uno/PracticaUno/PracticaDos/Memoria.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PracticaDos
+{
+    class Memoria
+    {
+        public void LeerMemoria()
+        {
+            string archivoDB = "../../../db.json";
+            StreamReader reader = new StreamReader(archivoDB);
+            var dbJSON = reader.ReadToEnd();
+            var dbObject = JObject.Parse(dbJSON);
+            // Prueba de lectura de archivo db.json
+            //var result = dbObject.ToString();
+            //var result = dbObject["arreglo"].ToString();
+            //var result = dbObject["arreglo"][0].ToString();
+            // Lectura de json iterable
+            foreach (var item in dbObject)
+            {
+                // Iteración individual de cada grupo de datos del objeto json.
+                Console.WriteLine("Dato en memoria:");
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
